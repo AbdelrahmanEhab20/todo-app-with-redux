b9c3fb69a05692f168934be58c8236b58e7cbb8c
AbdelrahmanEhab <abdoehab5008@gmail.com>
2024-05-16 21:14:50 +0300

    latest updates before deployment

--- src/components/SingleTodoItem.jsx
+++ src/components/SingleTodoItem.jsx
@@ -11,11 +11,11 @@ const SingleTodoItem = ({ todo, index }) => {
         sm:items-center justify-between border-b-2 py-2 gap-4">
             <div className="flex items-center">
                 <span className="mr-4 text-gray-500">{index + 1}</span>
-                <span className={`mr-1 w-5/5 ${todo.completed ? "line-through text-green-700" : " "}`}>{todo.text}</span>
+                <span className={`mr-4 ${todo.completed ? "line-through text-green-700" : " "}`}>{todo.text}</span>
             </div>
-            <div className="space-x-3 ml-8 w-4/5">
+            <div className="space-x-3 ml-8">
                 <button onClick={() => dispatch(toggleTodo(index))} className="mr-2 text-sm bg-blue-500 text-white sm:px-2 py-1 px-1 rounded">{todo.completed ? <FaToggleOff /> : <FaToggleOn />}</button>
-                <button onClick={() => dispatch(removeTodo(index))} className="mr-2 text-sm bg-red-500 text-white sm:px-2 py-1 px-1 rounded">  <FaTrash /> </button>
+                <button onClick={() => dispatch(removeTodo(index))} className="mr-2 text-sm bg-red-500 text-white sm:px-2 py-1 px-1 rounded">{<FaTrash />}</button>
                 {
                     !todo.completed && (<button onClick={() => dispatch(markCompleted(index))} className="mr-2 text-sm bg-green-500 text-white sm:px-2 py-1 px-1 rounded">{<FaCheck />}</button>)
                 }
