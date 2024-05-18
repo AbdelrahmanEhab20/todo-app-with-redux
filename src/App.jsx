e4504de61e26973145504bbf3eb870c35a8a7a5b
AbdelrahmanEhab <abdoehab5008@gmail.com>
2024-05-18 18:44:59 +0300

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import Todo from "./components/Todo";
-import './App.css';
 export default function App() {
   return (
     <Provider store={store}>
